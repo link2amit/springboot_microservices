ca769291c8dda657ba8e962748717ac97fbf3f3d
Amit Srivastava <asrivastava@watermarkinsights.com>
2020-02-03 08:47:54 +0530

    Added feign and ribbon

--- currency-conversion-service/src/main/java/com/microservices/CurrencyConversionApplication.java
+++ currency-conversion-service/src/main/java/com/microservices/CurrencyConversionApplication.java
@@ -2,8 +2,12 @@ package com.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@RibbonClient(name="currency-exchange-service")
 public class CurrencyConversionApplication {
 
 	public static void main(String[] args) {
--- currency-conversion-service/src/main/java/com/microservices/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/microservices/controller/CurrencyConversionController.java
@@ -1,34 +1,38 @@
 package com.microservices.controller;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.microservices.bean.ConversionBean;
+import com.microservices.feign.CurrencyExchangeProxy;
 
 @RestController
 public class CurrencyConversionController {
+
+	@Autowired
+	CurrencyExchangeProxy proxy;
 
 	@GetMapping("/currency_converters/from/{from}/to/{to}/quantity/{quantity}")
 	public ConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
 
-		Map<String, String> uriVariables = new HashMap<String, String>();
+		/*
+		 * Map<String, String> uriVariables = new HashMap<String, String>();
+		 * 
+		 * uriVariables.put("from", from); uriVariables.put("to", to);
+		 * ResponseEntity<ConversionBean> responseEntity = new
+		 * RestTemplate().getForEntity(
+		 * "http://localhost:8000/exchange_rates/from/{from}/to/{to}",
+		 * ConversionBean.class, uriVariables);
+		 */
+		ConversionBean response = proxy.getExchangeRate(from, to);
+//		responseEntity.getBody();
 
-		uriVariables.put("from", from);
-		uriVariables.put("to", to);
-		ResponseEntity<ConversionBean> responseEntity = new RestTemplate().getForEntity(
-				"http://localhost:8000/exchange_rates/from/{from}/to/{to}", ConversionBean.class, uriVariables);
-		ConversionBean response = responseEntity.getBody();
-
-		System.out.println(response);
-		return new ConversionBean(response.getId(), from, to, response.getExchangeRate(), response.getQuantity(),
+		return new ConversionBean(response.getId(), from, to, response.getExchangeRate(), quantity,
 				response.getExchangeRate().multiply(quantity), response.getPort());
 	}
 
